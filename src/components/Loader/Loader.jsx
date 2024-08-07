f355225e07b2f681cd2439e0da8689e64d583d23
Sikorskayaa <133200518+Sikorskayaa@users.noreply.github.com>
2024-08-07 14:47:40 +0300

    add-loader

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,6 +1,18 @@
-import css from "./Loader.module.css";
+import { ThreeDots } from "react-loader-spinner";
+
 const Loader = () => {
-  return <div className={css.loader}></div>;
+  return (
+    <ThreeDots
+      visible={true}
+      height="80"
+      width="80"
+      color="#fb8b03"
+      radius="9"
+      ariaLabel="three-dots-loading"
+      wrapperStyle={{ justifyContent: "center" }}
+      wrapperClass=""
+    />
+  );
 };
 
 export default Loader;
